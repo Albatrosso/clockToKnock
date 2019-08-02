8c2a52498797f3104c636b7ed8f8d42384fcb3f6
Nikita <nick.pavlenko98@gmail.com>
2019-08-02 17:21:39 +0300

    all

---
+++ timer.js
@@ -0,0 +1,42 @@
+const DEADLINE = 'August 13 2019 21:00:00 GMT+2:00';
+
+function getTimeRemaining (endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date());
+    let seconds = Math.floor( (t/1000)%60);
+    let minutes = Math.floor((t/1000/60)%60);
+    let hours = Math.floor((t/(1000*60*60))%24);
+    let days = Math.floor(t/(1000*60*60*24));
+    return{
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+
+}
+
+function initializeClock(endtime, id) {
+    let clock = document.getElementById(id);
+    let daysSpan = clock.querySelector('.days');
+    let hoursSpan = clock.querySelector('.hours');
+    let minutesSpan = clock.querySelector('.minutes');
+    let secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = ('0' + t.days+ ':').slice(-3);
+        hoursSpan.innerHTML = ('0' + t.hours+ ':').slice(-3);
+        minutesSpan.innerHTML = ('0' + t.minutes+ ':').slice(-3);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval)
+        }
+
+    }
+    updateClock();
+    let timeinterval = setInterval(updateClock,1000)
+}
+initializeClock(DEADLINE, 'clockdiv');
